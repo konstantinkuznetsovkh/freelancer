bc8cd9cd81309ddb46c0ba5b82750ed0af2d30e1
LightFortune <konstantinkuznetsovkh@gmail.com>
2020-01-23 20:42:32 +0200

    23012020204212

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     new WOW().init();
+
+    var mySwiper = new Swiper('.swiper-container', {
+
+        pagination: {
+            el: '.projects-pagination', //класс для обертки линков слайдера
+            bulletClass: 'project-bullet', //класс для линков слайдера
+            bulletActiveClass: 'project-bullet-active',
+            clickable: 'true' //кликабельные линки false не кликабельные
+        }
+    });
 })
